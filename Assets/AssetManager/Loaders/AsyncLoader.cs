8f3c9b4f78580abca08ac4872fb54783b79a21ab
duanhouhai <duanhouhai@shinezone.com>
2017-11-29 20:08:15 +0800

    t

---
+++ Assets/AssetManager/Loaders/AsyncLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YH.AssetManager
+{
+    public class AsyncLoader : ILoader
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/AssetManager/Loaders/EditorLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YH.AssetManager
+{
+    public class EditorLoader : ILoader
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
