999cfdf0ee1d687fd5a335ba9af31990b2b0fd0a
duanhouhai <duanhouhai@shinezone.com>
2017-12-14 20:03:07 +0800

    t

--- Assets/AssetManager/Editor/TestBuild.cs
+++ Assets/AssetManager/Editor/TestBuild.cs
@@ -7,7 +7,7 @@ using UnityEditor;
 public class TestBuild {
 
     [MenuItem("Test/TestBuildIos")]
-    public static void Test ()
+    public static void Test()
     {
         List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
 
@@ -15,46 +15,57 @@ public class TestBuild {
         string[] assets = new string[1];
         assets[0] = "Assets/ArtResources/Prefabs/TestPrefab.prefab";
 
-        build.assetBundleName = "mytest";
+        build.assetBundleName = "aaaaaaa";
         build.assetNames = assets;
 
         builds.Add(build);
 
         build = new AssetBundleBuild();
         assets = new string[1];
-        assets[0] = "Assets/ArtResources/Materials/MyMaterial";
-        build.assetBundleName = "mymat";
+        assets[0] = "Assets/ArtResources/Materials/MyMaterial.mat";
+        build.assetBundleName = "bbbbbb";
         build.assetNames = assets;
         builds.Add(build);
 
         build = new AssetBundleBuild();
         assets = new string[1];
         assets[0] = "Assets/ArtResources/Textures/bnt_Blue_S.png";
-        build.assetBundleName = "mypng";
+        build.assetBundleName = "ccccc";
         build.assetNames = assets;
         builds.Add(build);
 
 
         string outDir = Path.Combine(Application.dataPath, "../AssetBundles/Test");
 
-        AssetBundleManifest manifest= BuildPipeline.BuildAssetBundles(outDir, builds.ToArray(), BuildAssetBundleOptions.None,EditorUserBuildSettings.activeBuildTarget);
+        AssetBundleManifest manifest = BuildPipeline.BuildAssetBundles(outDir, builds.ToArray(), BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.DeterministicAssetBundle, EditorUserBuildSettings.activeBuildTarget);
 
-        Debug.Log("####All Bundles "+ manifest.GetAllAssetBundles().Length);
-        foreach(string v in manifest.GetAllAssetBundles())
+        Debug.Log("####All Bundles " + manifest.GetAllAssetBundles().Length);
+        foreach (string v in manifest.GetAllAssetBundles())
         {
             Debug.Log(v);
-            Debug.Log("########Dependencies "+ manifest.GetAllDependencies(v).Length);
+            Debug.Log("########Dependencies " + manifest.GetAllDependencies(v).Length);
             foreach (string k in manifest.GetAllDependencies(v))
             {
                 Debug.Log(k);
             }
 
-            Debug.Log("########Direct Dependencies "+ manifest.GetDirectDependencies(v).Length);
+            Debug.Log("########Direct Dependencies " + manifest.GetDirectDependencies(v).Length);
             foreach (string k in manifest.GetDirectDependencies(v))
             {
                 Debug.Log(k);
             }
         }
-        
+
+    }
+
+
+    [MenuItem("Test/TestDep")]
+    public static void TestDeps() {
+        string p = "Assets/ArtResources/Prefabs/TestPrefab.prefab";
+        var deps= AssetDatabase.GetDependencies(p,false);
+        foreach(string dep in deps)
+        {
+            Debug.Log(dep);
+        }
     }
 }
