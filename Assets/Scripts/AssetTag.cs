5d9fddf74d29ae0d6fd34bf6c68bcc7678f7e572
trarck <trarck@gmail.com>
2019-10-13 10:30:50 +0800

    修正错误和加入一个同引用的处理。

---
+++ Assets/Scripts/AssetTag.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+public class AssetTag
+{
+    public static int Main = 1;
+    public static int Battle = 2;
+
+}
+
