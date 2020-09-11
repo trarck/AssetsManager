08d0492eee46c97c44956dc083f736f148fbe7da
duanhouhai <houhai.duan@mihoyo.com>
2020-09-11 15:23:46 +0800

    重构loader释放方式。

--- Assets/AssetManager/Loaders/LoaderPool.cs
+++ Assets/AssetManager/Loaders/LoaderPool.cs
@@ -17,36 +17,92 @@ namespace YH.AssetManage
 		public static readonly ObjectPool<AssetBundleAsyncExistLoader> AssetBundleAsyncExistLoaderPool = new ObjectPool<AssetBundleAsyncExistLoader>(null, l => l.Clean());
 		public static readonly ObjectPool<AssetAsyncExistLoader> AssetAsyncExistLoaderPool = new ObjectPool<AssetAsyncExistLoader>(null, l => l.Clean());
 
+		private static Dictionary<Type, Action<Loader>> m_ReleaseLoaderTypeMap = InitReleaseLoaderTypeMap();
+
+		private static Dictionary<Type, Action<Loader>> InitReleaseLoaderTypeMap()
+		{
+			return new Dictionary<Type, Action<Loader>>(){
+				{typeof(AssetBundleAsyncLoader),ReleaseAssetBundleAsyncLoader},
+				{typeof(AssetAsyncLoader),ReleaseAssetAsyncLoader},
+				{typeof(AssetBundleSyncLoader),ReleaseAssetBundleSyncLoader},
+				{typeof(AssetSyncLoader),ReleaseAssetSyncLoader},
+				{typeof(AssetBundleAsyncExistLoader),ReleaseAssetBundleAsyncExistLoader},
+				{typeof(AssetAsyncExistLoader),ReleaseAssetAsyncExistLoader}
+			};	  
+		}
+
+		public static void ReleaseAssetBundleAsyncLoader(Loader loader)
+		{
+			AssetBundleAsyncLoaderPool.Release(loader as AssetBundleAsyncLoader);
+		}
+
+		public static void ReleaseAssetAsyncLoader(Loader loader)
+		{
+			AssetAsyncLoaderPool.Release(loader as AssetAsyncLoader);
+		}
+
+		public static void ReleaseAssetBundleSyncLoader(Loader loader)
+		{
+			AssetBundleSyncLoaderPool.Release(loader as AssetBundleSyncLoader);
+		}
+
+		public static void ReleaseAssetSyncLoader(Loader loader)
+		{
+			AssetSyncLoaderPool.Release(loader as AssetSyncLoader);
+		}
+
+		public static void ReleaseAssetBundleAsyncExistLoader(Loader loader)
+		{
+			AssetBundleAsyncExistLoaderPool.Release(loader as AssetBundleAsyncExistLoader);
+		}
+
+		public static void ReleaseAssetAsyncExistLoader(Loader loader)
+		{
+			AssetAsyncExistLoaderPool.Release(loader as AssetAsyncExistLoader);
+		}
+
 		public static void Release(Loader loader)
-        {
-			if (loader is AssetAsyncLoader)
+		{
+			
+			Type t = loader.GetType();
+			Action<Loader> act = null;
+			if (m_ReleaseLoaderTypeMap.TryGetValue(t, out act))
 			{
-				AssetAsyncLoaderPool.Release(loader as AssetAsyncLoader);
-			}
-			else if (loader is AssetBundleAsyncLoader)
-			{
-				AssetBundleAsyncLoaderPool.Release(loader as AssetBundleAsyncLoader);
-			}
-			else if (loader is AssetBundleSyncLoader)
-			{
-				AssetBundleSyncLoaderPool.Release(loader as AssetBundleSyncLoader);
-			}
-			else if (loader is AssetSyncLoader)
-			{
-				AssetSyncLoaderPool.Release(loader as AssetSyncLoader);
-			}
-			else if (loader is AssetBundleAsyncExistLoader)
-			{
-				AssetBundleAsyncExistLoaderPool .Release(loader as AssetBundleAsyncExistLoader);
-			}
-			else if (loader is AssetAsyncExistLoader)
-			{
-				AssetAsyncExistLoaderPool.Release(loader as AssetAsyncExistLoader);
+				act(loader);
 			}
 			else
 			{
 				loader.Clean();
 			}
+
+			//if (loader is AssetBundleAsyncExistLoader)
+			//{
+			//	AssetBundleAsyncExistLoaderPool.Release(loader as AssetBundleAsyncExistLoader);
+			//}
+			//else if (loader is AssetAsyncExistLoader)
+			//{
+			//	AssetAsyncExistLoaderPool.Release(loader as AssetAsyncExistLoader);
+			//}
+			//else if (loader is AssetAsyncLoader)
+			//{
+			//	AssetAsyncLoaderPool.Release(loader as AssetAsyncLoader);
+			//}
+			//else if (loader is AssetBundleAsyncLoader)
+			//{
+			//	AssetBundleAsyncLoaderPool.Release(loader as AssetBundleAsyncLoader);
+			//}
+			//else if (loader is AssetBundleSyncLoader)
+			//{
+			//	AssetBundleSyncLoaderPool.Release(loader as AssetBundleSyncLoader);
+			//}
+			//else if (loader is AssetSyncLoader)
+			//{
+			//	AssetSyncLoaderPool.Release(loader as AssetSyncLoader);
+			//}
+			//else
+			//{
+			//	loader.Clean();
+			//}
         }        
     }
 }
