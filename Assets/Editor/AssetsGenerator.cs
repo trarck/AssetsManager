fa622329242d1f18f469729b98e1cc5fd54338be
duanhouhai <houhai.duan@mihoyo.com>
2021-05-28 21:14:12 +0800

    添加自动生成测试文件。

---
+++ Assets/Editor/AssetsGenerator.cs
@@ -0,0 +1,442 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using YH;
+
+public class StringGenerator
+{
+    private const string BasicString = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    private const int BasicStringLen = 62;
+    public static string RandomString(int len)
+    {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < len; ++i)
+        {
+            sb.Append(BasicString[Random.Range(0, BasicStringLen)]);
+        }
+        return sb.ToString();
+    }
+}
+
+[System.Serializable]
+public class GenerateAssetConfig
+{
+    public enum ItemType
+    {
+        Image,
+        Material,
+        Prefab
+    }
+
+    public string name;
+    public ItemType itemType;
+    public string baseFolder;
+    public string outFolder;
+    public int generateCount;
+    public string fileFilter = "*";
+
+    public bool useSubFolder = true;
+    public int maxFilePerFolder = 1000;
+
+    public int folderRandomMinLen = 4;
+    public int folderRandomMaxLen = 8;
+
+    public int fileRandomMinLen = 6;
+    public int fileRandomMaxLen = 20;
+
+    public string needResultName;
+}
+
+public class AssetGenerator
+{
+    public GenerateAssetConfig config;
+
+    protected List<string> m_Items;
+
+    public System.Action<float,int> onProgress;
+
+    public List<string> items
+    {
+        get
+        {
+            return m_Items;
+        }
+        set
+        {
+            m_Items = value;
+        }
+    }
+
+    public AssetGenerator()
+    {
+
+    }
+
+    public AssetGenerator(GenerateAssetConfig config)
+    {
+        this.config = config;
+    }
+
+    public virtual void BeforeGenerate()
+    {
+        if (m_Items == null)
+        {
+            m_Items = new List<string>();
+        }
+        else
+        {
+            m_Items.Clear();
+        }
+
+        AssetDatabase.StartAssetEditing();
+    }
+
+    public virtual void GenerateStep(string sourceFile, string outFile)
+    {
+        m_Items.Add(outFile);
+    }
+
+    public virtual void AfterGenerate()
+    {
+        AssetDatabase.StopAssetEditing();
+        AssetDatabase.Refresh();
+    }
+
+    public virtual void Generate()
+    {
+        string[] origFiles = Directory.GetFiles(config.baseFolder, config.fileFilter, SearchOption.AllDirectories);
+        int originCount = origFiles.Length;
+
+        BeforeGenerate();
+
+        string folderName = "";
+        int folderFileCount = 0;
+
+        for (int i = 0; i < config.generateCount; ++i)
+        {
+            string sourceFile = origFiles[i % originCount];
+            string ext = Path.GetExtension(sourceFile);
+
+
+            if (config.useSubFolder)
+            {
+                if (folderFileCount == 0)
+                {
+                    folderName = GetFolderRandomString();
+                }
+
+                if (++folderFileCount > config.maxFilePerFolder)
+                {
+                    folderFileCount = 0;
+                }
+            }
+
+            string outFile = Path.Combine(config.outFolder, folderName, GetFileRandomString()) + ext;
+            string outDir = Path.GetDirectoryName(outFile);
+            if (!Directory.Exists(outDir))
+            {
+                Directory.CreateDirectory(outDir);
+            }
+            GenerateStep(sourceFile,outFile);
+            if (onProgress != null)
+            {
+                onProgress(i / (float)config.generateCount, i);
+            }
+        }
+        AfterGenerate();
+    }
+
+    public void LoadItems()
+    {
+        if (m_Items == null)
+        {
+            m_Items = new List<string>();
+        }
+        else
+        {
+            m_Items.Clear();
+        }
+        string[] generateFiles = Directory.GetFiles(config.baseFolder, config.fileFilter, SearchOption.AllDirectories);
+        m_Items.AddRange(generateFiles);
+    }
+
+    protected string GetFolderRandomString()
+    {
+        int len = Random.Range(config.fileRandomMinLen, config.folderRandomMaxLen + 1);
+        return StringGenerator.RandomString(len);
+    }
+
+    protected string GetFileRandomString()
+    {
+        int len = Random.Range(config.fileRandomMinLen, config.fileRandomMaxLen + 1);
+        return StringGenerator.RandomString(len);
+    }
+}
+
+public class TextureGenerator : AssetGenerator
+{
+
+    public TextureGenerator(GenerateAssetConfig config) : base(config)
+	{
+
+	}
+
+	public override void GenerateStep(string sourceFile, string outFile)
+	{
+        File.Copy(sourceFile, outFile);
+        base.GenerateStep(sourceFile, outFile);
+	}
+}
+
+public class MaterialGenerator : AssetGenerator
+{
+    private List<string> m_TempTexturePropertyNames = new List<string>();
+    private List<string> m_ImageFiles;
+
+    public MaterialGenerator(GenerateAssetConfig config, Dictionary<string,List<string>> results)
+    {
+        this.config = config;
+        m_ImageFiles = results[config.needResultName];
+    }
+
+    public override void GenerateStep(string sourceFile, string outFile)
+    {
+        string assetPath = FileSystem.AddAssetPrev(FileSystem.Relative(Application.dataPath, sourceFile));
+        Material baseMat = AssetDatabase.LoadAssetAtPath<Material>(assetPath);
+
+        Material outMat = new Material(baseMat);
+        SetMaterialTexture(outMat);
+        AssetDatabase.CreateAsset(outMat, outFile);
+        base.GenerateStep(sourceFile, outFile);
+    }
+
+    private Texture GetRandomTexture()
+    {
+        int i = Random.Range(0, m_ImageFiles.Count);
+        string imageFile = m_ImageFiles[i];
+        string assetPath = FileSystem.AddAssetPrev(FileSystem.Relative(Application.dataPath, imageFile));
+        Texture tex = AssetDatabase.LoadAssetAtPath<Texture>(assetPath);
+        return tex;
+    }
+
+    private void SetMaterialTexture(Material mat)
+    {
+        m_TempTexturePropertyNames.Clear();
+        mat.GetTexturePropertyNames(m_TempTexturePropertyNames);
+        foreach (var texName in m_TempTexturePropertyNames)
+        {
+            if (texName.Contains("NormalMap") || texName.Contains("BumpMap"))
+            {
+                continue;
+            }
+            mat.SetTexture(texName, GetRandomTexture());
+        }
+    }
+}
+
+public class PrefabGenerator : AssetGenerator
+{
+    private List<string> m_MaterialFiles;
+
+    public PrefabGenerator(GenerateAssetConfig config, Dictionary<string,List<string>> results)
+    {
+        this.config = config;
+        m_MaterialFiles = results[config.needResultName];
+    }
+
+	public override void GenerateStep(string sourceFile, string outFile)
+    {
+        string assetPath = FileSystem.AddAssetPrev(FileSystem.Relative(Application.dataPath, sourceFile));
+        GameObject basePrefab = AssetDatabase.LoadAssetAtPath<GameObject>(assetPath);
+
+        GameObject newPrefab = PrefabUtility.InstantiatePrefab(basePrefab) as GameObject;
+
+        Renderer[] renderers = newPrefab.GetComponentsInChildren<Renderer>(true);
+        foreach (var r in renderers)
+        {
+            Material[] mats = new Material[r.sharedMaterials.Length];
+            for (int i = 0; i < r.sharedMaterials.Length; ++i)
+            {
+                mats[i] = GetRandomMaterial();
+            }
+            r.sharedMaterials = mats;
+        }
+
+        PrefabUtility.SaveAsPrefabAsset(newPrefab, outFile);
+        GameObject.DestroyImmediate(newPrefab);
+        base.GenerateStep(sourceFile, outFile);
+    }
+
+    private Material GetRandomMaterial()
+    {
+        int i = Random.Range(0, m_MaterialFiles.Count);
+        string imageFile = m_MaterialFiles[i];
+        string assetPath = FileSystem.AddAssetPrev(FileSystem.Relative(Application.dataPath, imageFile));
+        Material mat = AssetDatabase.LoadAssetAtPath<Material>(assetPath);
+        return mat;
+    }
+
+}
+
+
+public class AssetsGenerator 
+{
+    public List<GenerateAssetConfig> generateAssetConfigs;
+
+    public Dictionary<string, List<string>> results;
+
+    public System.Action<string,float, int> onProgress;
+
+    public void GenerateAll()
+    {
+        results = new Dictionary<string, List<string>>();
+
+        foreach (var config in generateAssetConfigs)
+        {
+            AssetGenerator generator = null;
+
+            switch (config.itemType)
+            {
+                case GenerateAssetConfig.ItemType.Image:
+                    generator = new TextureGenerator(config);
+                    break;
+                case GenerateAssetConfig.ItemType.Material:
+                    generator = new MaterialGenerator(config,results);
+                    break;
+                case GenerateAssetConfig.ItemType.Prefab:
+                    generator = new PrefabGenerator(config, results);
+                    break;
+            }
+
+
+            if (onProgress != null)
+            {
+                generator.onProgress += (p, i) =>
+                {
+                    onProgress(generator.config.name,p, i);
+                };
+            }
+
+            generator.Generate();
+            results[config.name] = generator.items;
+        }
+    }
+}
+
+public class GenerateAssetsWindow : EditorWindow
+{
+    public class GenerateConfigs : ScriptableObject
+    {
+        public List<GenerateAssetConfig> configs=new List<GenerateAssetConfig>();
+    }
+
+    [System.Serializable]
+    public class SerializeableConfigs 
+    {
+        public List<GenerateAssetConfig> configs = new List<GenerateAssetConfig>();
+    }
+
+    SerializedObject m_ConfigsObj;
+
+    [MenuItem("Tools/GenerateLargeAmountAssets")]
+    public static void Open()
+    {
+        GetWindow<GenerateAssetsWindow>(false, "Generate Assets");
+    }
+
+	private void OnEnable()
+	{
+        GenerateConfigs obj = ScriptableObject.CreateInstance<GenerateConfigs>();
+        m_ConfigsObj = new SerializedObject(obj);
+    }
+
+	private void OnGUI()
+	{
+        m_ConfigsObj.Update();
+
+        SerializedProperty prop = m_ConfigsObj.GetIterator();
+
+        while (prop.NextVisible(true))
+        {
+            if (prop.depth != 0)
+                continue;
+
+            if (prop.name.EndsWith("Script"))
+                continue;
+
+            EditorGUILayout.PropertyField(prop, true);
+        }
+        m_ConfigsObj.ApplyModifiedProperties();
+
+
+        EditorGUILayout.BeginHorizontal();
+        {
+            if (GUILayout.Button("Save"))
+            {
+                SaveConfigs();
+            }
+
+            if (GUILayout.Button("Load"))
+            {
+                LoadConfigs();
+            }
+        }
+        EditorGUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Generate"))
+        {
+            Generate();
+        }
+    }
+
+    private string GetSaveConfigFile()
+    {
+        string dir = Path.Combine(Application.dataPath, "../AssetDatabase");
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+        return Path.Combine(dir, "GenerateConfig.json");
+    }
+
+    private void SaveConfigs()
+    {
+        GenerateConfigs generateConfigs = m_ConfigsObj.targetObject as GenerateConfigs;
+        if (generateConfigs)
+        {
+            SerializeableConfigs saveConfigs = new SerializeableConfigs();
+            saveConfigs.configs = generateConfigs.configs;
+            string context = JsonUtility.ToJson(saveConfigs);
+            File.WriteAllText(GetSaveConfigFile(), context);
+        }
+    }
+
+    private void LoadConfigs()
+    {
+        string context = File.ReadAllText(GetSaveConfigFile());
+        SerializeableConfigs saveConfigs = JsonUtility.FromJson<SerializeableConfigs>(context);
+        GenerateConfigs generateConfigs = m_ConfigsObj.targetObject as GenerateConfigs;
+        generateConfigs.configs = saveConfigs.configs;
+    }
+
+    private void Generate()
+    {
+        GenerateConfigs generateConfigs = m_ConfigsObj.targetObject as GenerateConfigs;
+        if (generateConfigs)
+        {
+            AssetsGenerator assetsGenerator = new AssetsGenerator();
+            assetsGenerator.generateAssetConfigs = generateConfigs.configs;
+            assetsGenerator.onProgress += ShowGenerateProgress;
+            assetsGenerator.GenerateAll();
+            EditorUtility.ClearProgressBar();
+        }
+    }
+
+    private void ShowGenerateProgress(string name, float percent, int idx)
+    {
+        EditorUtility.DisplayProgressBar("Genereate " + name, idx.ToString(), percent);
+    }
+}
---
+++ Assets/Scripts/FileSystem.cs
@@ -0,0 +1,464 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using UnityEngine;
+
+
+namespace YH
+{
+    public class FileSystem
+    {
+
+        public static string dataPath = Application.dataPath;
+
+        public static string streamingAssetsPath = Application.streamingAssetsPath;
+
+        public static string bundlesPath="AssetBundles";
+
+        public static string bundleManifestFile = "all.manifest";
+
+        public static string bundleOutPaths = "AssetBundles";
+
+        public static string assetPathPrev = "Assets/";
+
+        public static List<string> searchPaths = new List<string>();
+
+        /// <summary>
+        /// 获取文件的全路径
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string FullPathForFilename(string filename)
+        {
+            if (string.IsNullOrEmpty(filename))
+            {
+                return filename;
+            }
+
+            if (Path.IsPathRooted(filename))
+            {
+                return filename;
+            }
+
+            //get from search path
+            var fullPath = GetFullPathFromSearchPaths(filename);
+            if (!string.IsNullOrEmpty(fullPath))
+            {
+                return fullPath;
+            }
+
+            //streaming path
+            return StreamingPathForFilename(filename);
+        }
+
+        /// <summary>
+        /// 在搜索目录查找文件
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string GetFullPathFromSearchPaths(string filename)
+        {
+            foreach (string searchPath in searchPaths)
+            {
+                string fullPath = Path.Combine(searchPath, filename);
+                if (File.Exists(fullPath))
+                {
+                    return fullPath;
+                }
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 添加一个搜索路径
+        /// </summary>
+        /// <param name="path"></param>
+        public static void AddSearchPath(string path)
+        {
+            if (!searchPaths.Contains(path))
+            {
+                searchPaths.Add(path);
+            }
+        }
+
+        /// <summary>
+        /// 插入一个搜索路径
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="path"></param>
+        public static void InsertSearchPath(int index,string path)
+        {
+            if (!searchPaths.Contains(path))
+            {
+                searchPaths.Insert(index, path);
+            }
+        }
+
+        /// <summary>
+        /// 插入一组搜索路径
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="paths"></param>
+        public static void InsertRangeSearchPath(int index, string[] paths)
+        {
+            searchPaths.InsertRange(index, paths);
+        }
+
+        /// <summary>
+        /// 移除搜索路径
+        /// </summary>
+        /// <param name="path"></param>
+        public static void RemoveSearchPath(string path)
+        {
+            if (!searchPaths.Contains(path))
+            {
+                searchPaths.Remove(path);
+            }
+        }
+
+        /// <summary>
+        /// 直接获取streaming的url
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string StreamingUrlForFilename(string filename)
+        {
+#if UNITY_ANROID
+            return "jar:file://" + Application.dataPath + "!/assets/" + filename;
+#else
+            return "file://" + Combine(Application.streamingAssetsPath, filename);
+#endif
+        }
+
+        /// <summary>
+        /// 获取Streaming下文件路径
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string StreamingPathForFilename(string filename)
+        {
+            return Combine(Application.streamingAssetsPath, filename);
+        }
+
+        /// <summary>
+        /// 合并路径
+        /// </summary>
+        /// <param name="paths"></param>
+        /// <returns></returns>
+        public static string Combine(params string[] paths)
+        {
+            if (paths.Length == 0) return string.Empty;
+            if (paths.Length == 1) return paths[0];
+
+            StringBuilder sb = new StringBuilder();
+
+            string c=null,n=null;            
+
+            int start = 0;
+            for (; start < paths.Length; ++start)
+            {
+                c = paths[start];
+                if (!string.IsNullOrEmpty(c))
+                {
+                    sb.Append(c);
+                    break;
+                }
+            }            
+
+            for(int i = start+1; i < paths.Length; ++i)
+            {
+                n = paths[i];
+                if (string.IsNullOrEmpty(n))
+                {
+                    continue;
+                }
+    
+                if((c.EndsWith("/") || c.EndsWith("\\")))
+                {
+                    if(n.StartsWith("/") || n.StartsWith("\\"))
+                    {
+                        sb.Append(n.Substring(1));
+                    }
+                    else
+                    {
+                        sb.Append(n);
+                    }
+                }
+                else
+                {
+                    if (n.StartsWith("/") || n.StartsWith("\\"))
+                    {
+                        sb.Append(n);
+                    }
+                    else
+                    {
+                        sb.Append(Path.DirectorySeparatorChar);
+                        sb.Append(n);
+                    }
+                }
+                c = n;
+            }
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// 归一化路径
+        /// </summary>
+        /// <param name="filename"></param>
+        /// <returns></returns>
+        public static string NormalizeFilename(string filename)
+        {
+            return filename.Replace("\\", "/");
+        }
+
+        /// <summary>
+        /// 获取路径的相对位置
+        /// </summary>
+        /// <param name="fromPath"></param>
+        /// <param name="toPath"></param>
+        /// <returns></returns>
+        public static string Relative(string fromPath, string toPath)
+        {
+            fromPath = fromPath.Replace("\\", "/");
+            toPath = toPath.Replace("\\", "/");
+
+            if (fromPath[fromPath.Length - 1] == '/')
+            {
+                fromPath = fromPath.Substring(0, fromPath.Length - 1);
+            }
+
+            if (toPath[toPath.Length - 1] == '/')
+            {
+                toPath = toPath.Substring(0, toPath.Length - 1);
+            }
+
+            string[] froms = fromPath.Split('/');
+            string[] tos = toPath.Split('/');
+
+            int i = 0;
+            //look for same part
+            for (int l=froms.Length>tos.Length?tos.Length: froms.Length; i < l; ++i)
+            {
+                if (froms[i] != tos[i])
+                {
+                    break;
+                }
+            }
+
+            if (i == 0)
+            {
+                //just windows. eg.fromPath=c:\a\b\c,toPath=d:\e\f\g
+                //if linux the first is empty always same. eg. fromPath=/a/b/c,toPath=/d/e/f
+                return toPath;
+            }
+            else
+            {
+                System.Text.StringBuilder result = new System.Text.StringBuilder();
+                System.Text.StringBuilder toSB = new System.Text.StringBuilder();
+
+                for (int j = i; j < froms.Length; ++j)
+                {
+                    result.Append("../");
+                }
+
+                for (int j = i; j < tos.Length; ++j)
+                {
+                    result.Append(tos[j]);
+                    if (j < tos.Length - 1)
+                    {
+                        result.Append("/");
+                    }
+                }
+                return result.ToString();
+            }
+        }
+
+        /// <summary>
+        /// 移除路径中的Assets
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string RemoveAssetPrev(string path)
+        {
+            if (!string.IsNullOrEmpty(path) && path.StartsWith(assetPathPrev,System.StringComparison.CurrentCultureIgnoreCase))
+            {
+                return path.Substring(assetPathPrev.Length);
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// 添加Assets到路径。如果存在则不添加。
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        public static string AddAssetPrev(string path)
+        {
+            if (!string.IsNullOrEmpty(path) && !path.StartsWith(assetPathPrev, System.StringComparison.CurrentCultureIgnoreCase))
+            {
+                return Combine(assetPathPrev, path);
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// 强制删除一个目录。包括自己
+        /// </summary>
+        /// <param name="path"></param>
+        public static void ForceDeleteDirectory(string path)
+        {
+            DirectoryInfo root;
+            Stack<DirectoryInfo> fols;
+            DirectoryInfo fol;
+            fols = new Stack<DirectoryInfo>();
+            root = new DirectoryInfo(path);
+            fols.Push(root);
+            while (fols.Count > 0)
+            {
+                fol = fols.Pop();
+                fol.Attributes = fol.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                foreach (DirectoryInfo d in fol.GetDirectories())
+                {
+                    fols.Push(d);
+                }
+                foreach (FileInfo f in fol.GetFiles())
+                {
+                    f.Attributes = f.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                    f.Delete();
+                }
+            }
+            root.Delete(true);
+        }
+
+        /// <summary>
+        /// 清空一个目录。
+        /// </summary>
+        /// <param name="path"></param>
+        public static void ForceClearDirectory(string path)
+        {
+            DirectoryInfo root;
+            Stack<DirectoryInfo> fols;
+            DirectoryInfo fol;
+            fols = new Stack<DirectoryInfo>();
+            root = new DirectoryInfo(path);
+            fols.Push(root);
+            while (fols.Count > 0)
+            {
+                fol = fols.Peek();
+                fol.Attributes = fol.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+
+                foreach (FileInfo f in fol.GetFiles())
+                {
+                    f.Attributes = f.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                    f.Delete();
+                }
+
+                DirectoryInfo[] subs = fol.GetDirectories();
+                if (subs.Length > 0)
+                {
+                    foreach (DirectoryInfo d in fol.GetDirectories())
+                    {
+                        fols.Push(d);
+                    }
+                }
+                else
+                {
+                    if (fol != root)
+                    {
+                        fol.Delete(true);
+                    }
+
+                    fols.Pop();
+                }
+            }
+        }
+
+        /// <summary>
+        /// 按照规则移除一个目录下的文件。
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="pattern"></param>
+        public static void RemoveDirectoryFiles(string path, string pattern)
+        {
+            DirectoryInfo root;
+            Stack<DirectoryInfo> fols;
+            DirectoryInfo fol;
+            fols = new Stack<DirectoryInfo>();
+            root = new DirectoryInfo(path);
+            fols.Push(root);
+            System.Text.RegularExpressions.Regex reg = new System.Text.RegularExpressions.Regex(pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+
+            while (fols.Count > 0)
+            {
+                fol = fols.Peek();
+                foreach (FileInfo f in fol.GetFiles())
+                {
+                    if (reg.IsMatch(f.Name))
+                    {
+                        f.Attributes = f.Attributes & ~(FileAttributes.Archive | FileAttributes.ReadOnly | FileAttributes.Hidden);
+                        f.Delete();
+                    }
+                }
+
+                DirectoryInfo[] subs = fol.GetDirectories();
+                if (subs.Length > 0)
+                {
+                    foreach (DirectoryInfo d in fol.GetDirectories())
+                    {
+                        fols.Push(d);
+                    }
+                }
+                else
+                {
+                    fols.Pop();
+                }
+            }
+        }
+        
+        public List<string> SearchFiles(string path, string pattern = null)
+        {
+            DirectoryInfo startInfo = new DirectoryInfo(path);
+            if (!startInfo.Exists)
+            {
+                return null;
+            }
+
+            List<string> result = new List<string>();
+
+            Stack<DirectoryInfo> dirs = new Stack<DirectoryInfo>();
+            dirs.Push(startInfo);
+
+            DirectoryInfo dir;
+
+            bool haveFilter = false;
+            System.Text.RegularExpressions.Regex reg = null;
+            if (!string.IsNullOrEmpty(pattern))
+            {
+                haveFilter = true;
+                reg = new System.Text.RegularExpressions.Regex(pattern, System.Text.RegularExpressions.RegexOptions.IgnoreCase);
+            }
+
+            while (dirs.Count > 0)
+            {
+                dir = dirs.Pop();
+
+                foreach (FileInfo fi in dir.GetFiles())
+                {
+                    if (!haveFilter || reg.IsMatch(fi.FullName))
+                    {
+                        result.Add(fi.FullName);
+                    }
+                }
+
+                foreach (DirectoryInfo subDir in dir.GetDirectories())
+                {
+                    if (!subDir.Name.StartsWith("."))
+                    {
+                        dirs.Push(subDir);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
