df5ed042bc914dcb75af9848a31f52bc46801a78
trarck <trarck@gmail.com>
2022-02-06 21:57:08 +0800

    t

--- Assets/Tests/Scripts/TestLoadPerformance.cs
+++ Assets/Tests/Scripts/TestLoadPerformance.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using YH.AssetManage;
+using System;
 
 public class TestLoadPerformance : MonoBehaviour
 {
@@ -13,13 +14,59 @@ public class TestLoadPerformance : MonoBehaviour
     {
         m_AssetManager.Init(null, (r) =>
         {
-            DoTest();
         });
+    }
+
+    void OnGUI()
+    {
+        if (GUILayout.Button("AllInOne"))
+        {
+            TestAllInOne();
+        }
+
+        if (GUILayout.Button("SingleAll"))
+        {
+            TestAllInOne();
+        }
     }
 
     // Update is called once per frame
     void DoTest()
     {
+        TestSingleAll();
+    }
+
+    void TestAllInOne()
+    {
+        string allInOne = "bigprefab_allInOne.ab";
+        DateTime start = DateTime.Now;
         
+        m_AssetManager.LoadAsset<GameObject>(allInOne,(ar)=>
+        {
+            DateTime end = DateTime.Now;
+            TimeSpan used = end - start;
+            Debug.LogFormat("used:{0}", used.TotalMilliseconds);
+            if (ar!=null)
+            {
+                Debug.Log(ar);
+            }
+        });
+    }
+
+    void TestSingleAll()
+    {
+        string allInOne = "bigprefab_singleAll.ab";
+        DateTime start = DateTime.Now;
+
+        m_AssetManager.LoadAsset<GameObject>(allInOne, (ar) =>
+        {
+            DateTime end = DateTime.Now;
+            TimeSpan used = end - start;
+            Debug.LogFormat("used:{0}", used.TotalMilliseconds);
+            if (ar != null)
+            {
+                Debug.Log(ar);
+            }
+        });
     }
 }
